65b14afd3523f10c13939f04ad385f12fd9e815b
Patricia O'Brien <patriciaobrien@wireless-10-145-211-206.public.utexas.edu>
2015-09-15 21:21:45 -0500

    no message

--- 01 Data/Dataframe1.R
+++ 01 Data/Dataframe1.R
@@ -3,7 +3,6 @@ require("RCurl")
 require("ggplot2")
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mmc2762', PASS='orcl_mmc2762', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
 summary(df)
 head(df)
   
